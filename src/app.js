30a3e1746cf29584137f4892ea3a291f216e3b4c
Shi Tan <shi.tan@kaiostech.com>
2020-12-15 06:42:35 +0000

    Revert "add me page"
    This reverts commit fa5a122507347465a477afc66364d7723adafa1e

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import { Provider } from 'react-redux';
 
 import store from './redux/store';
 import Home from './pages/home';
-import Me from './pages/Me';
 import Playlist from './pages/playlist';
 import Player from './pages/player';
 
@@ -25,7 +24,7 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <Route exact path='/playlist' component={Playlist} />
             <Route exact path='/player' component={Player} />
-            <Route exact path='/me' component={Me} />
+
             <Redirect to='/' />
           </Switch>
         </Router>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -24,9 +24,6 @@ export default class Home extends Component {
       case 'Enter':
         this.props.history.push('/player');
         break;
-      case '0':
-        this.props.history.push('/me');
-        break;
       case 'ArrowDown':
         this.props.history.push('/playlist');
         break;
@@ -39,7 +36,6 @@ export default class Home extends Component {
     return (
       <div ref={ref => {this.element = ref}} onKeyDown={this.handleKeyDown} className="home" tabIndex="-1">
         <span>Press Enter key into Player Screen</span>
-        <span>Click <b>number '0' </b> key to jump to <b>Me</b> page</span>
         <span>Press ArrowDown key into Playlist Screen</span>
       </div>
     )
